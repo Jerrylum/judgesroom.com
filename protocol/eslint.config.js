6a97431f4d3c61f90ce9dbf831a86867f93c6686
Jerry Lum <serverofjerry@gmail.com>
2025-08-14 11:41:23 -0700

    Initial protocol subproject

---
+++ protocol/eslint.config.js
@@ -0,0 +1,36 @@
+import prettier from 'eslint-config-prettier';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+const prettierIgnorePath = fileURLToPath(new URL('./.prettierignore', import.meta.url));
+
+export default ts.config(
+	includeIgnoreFile(gitignorePath, 'gitignore'),
+	includeIgnoreFile(prettierIgnorePath, 'prettierignore'),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	prettier,
+	{
+		languageOptions: {
+			globals: { ...globals.node, ...globals['shared-node-browser'] }
+		},
+		rules: {
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off',
+			'@typescript-eslint/no-unused-vars': 'off'
+		}
+	},
+	{
+		files: ['**/*.ts'],
+		languageOptions: {
+			parserOptions: {
+				projectService: true
+			}
+		}
+	}
+);
