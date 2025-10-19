8096c4f7ef38fcc03e033f6e7eacc18691381842
Jerry Lum <serverofjerry@gmail.com>
2025-10-19 22:56:59 +0800

    Add 404 error page component with title and message for page not found

---
+++ web/src/routes/404/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const props = $props();
+</script>
+
+<svelte:head>
+	<title>404 | Judges' Room</title>
+</svelte:head>
+
+<div class="min-h-screen">
+	<div class="flex h-screen flex-col bg-slate-100">
+		<div class="flex flex-1 flex-col items-center justify-center p-8">
+			<h1 class="text-4xl font-bold">404</h1>
+			<p class="text-lg">Page not found</p>
+		</div>
+	</div>
+</div>
