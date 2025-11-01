fe91e2089984bab554c5f98ae35e80839d9fea29
Jerry Lum <serverofjerry@gmail.com>
2025-11-01 16:18:52 +0800

    Update version number to v0.1.1

--- web/src/lib/app.svelte.ts
+++ web/src/lib/app.svelte.ts
@@ -106,7 +106,7 @@ export class App {
 	private allJudges: readonly Judge[] = $state([]);
 	private allDevices: readonly DeviceInfo[] = $state([]);
 	private allFinalAwardNominations: Record<string, AwardNomination[]> = $state({});
-	public readonly version: string = '0.1.0';
+	public readonly version: string = '0.1.1';
 
 	// Error handling
 	private notices: Notice[] = $state([]);
