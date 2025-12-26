37200dc5564ae312ee29005307b833dd0e4d11dd
Jerry Lum <serverofjerry@gmail.com>
2025-12-26 14:06:16 +0800

    Install paraglide

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>
+			{locale}
+		</a>
+	{/each}
+</div>
